ddc8f3a6901b3dcdd2ba202d70ca8f6a8a896d4d
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-12-12 20:11:16 +0000

    init and login

---
+++ login.php
@@ -0,0 +1,5 @@
+<form action="">
+  <input type="text" name="username" placeholder="user">
+  <input type="password" name="password" placeholder="password">
+  <input type="submit" value="entrar">
+</form>
